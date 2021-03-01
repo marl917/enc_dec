41c924caf6259283b8f64b97ab792fad18b8e6e4
marl917 <marlene.careil123@gmail.com>
2021-03-01 10:07:49 +0100

    modify reg loss

--- gan_training/train_BiGAN.py
+++ gan_training/train_BiGAN.py
@@ -181,7 +181,7 @@ class Trainer(object):
         reg = torch.tensor(0., device='cuda')
         if self.reg_type == 'real' or self.reg_type == 'real_fake':
             dloss_real.backward(retain_graph=True)
-            reg = self.reg_param * compute_grad2(d_real[1], x_real).mean()
+            reg = self.reg_param * compute_grad2(d_real[1], [x_real, label_map]).mean()
             reg.backward()
         else:
             dloss_real.backward()
@@ -244,14 +244,14 @@ def toggle_grad(model, requires_grad):
         p.requires_grad_(requires_grad)
 
 def compute_grad2(d_out, x_in):
-    batch_size = x_in.size(0)
+    batch_size = x_in[0].size(0)
     grad_dout = autograd.grad(outputs=d_out.sum(),
                               inputs=x_in,
                               create_graph=True,
                               retain_graph=True,
                               only_inputs=True)[0]
     grad_dout2 = grad_dout.pow(2)
-    assert (grad_dout2.size() == x_in.size())
+    assert (grad_dout2.size() == x_in[0].size())
     reg = grad_dout2.view(batch_size, -1).sum(1)
     return reg
 
